576994bbccc29d235c3d97623ccfdb162ba54546
kaikai <gamera1000x@gmail.com>
2023-12-28 23:19:52 +0900

    編集画面で、ユーザーの情報を編集できるようにした

---
+++ swiftDataPractice/SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  swiftDataPractice
+//
+//  Created by 青山凱 on 2023/12/27.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
